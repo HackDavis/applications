71dd019a994cedb561109b452750acbf5055e322
klanmiko <eternity01033@gmail.com>
2018-06-20 21:55:45 -0700

    automate requirements.txt through pip freeze

--- start.py
+++ start.py
@@ -6,7 +6,8 @@ commands = {
     'run-windows': "set FLASK_APP=app.py; flask run",
     'install': "pip3 install -r requirements.txt",
     'format': "autopep8 --in-place --aggressive --aggressive *.py",
-    'deploy': "pm2 start start.sh"
+    'deploy': "pm2 start start.sh",
+    'publish-deps': 'pip3 freeze > requirements.txt'
 }
 
 parser = argparse.ArgumentParser(description='Manage the Flask App')

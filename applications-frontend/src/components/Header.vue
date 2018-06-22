a7277f29f95acad09f72a65cdf0e2a7c92dc8d34
klanmiko <eternity01033@gmail.com>
2018-06-21 21:53:06 -0700

    remove helloworld and add vue router

---
+++ applications-frontend/src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+        <nav class="navbar has-shadow is-spaced" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <router-link class="navbar-item" to="/">
+                    <img src="@/assets/logo.png">
+                </router-link>
+
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                </a>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    created() {
+        document.addEventListener('DOMContentLoaded', function () {
+
+            // Get all "navbar-burger" elements
+            var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+            // Check if there are any navbar burgers
+            if ($navbarBurgers.length > 0) {
+
+                // Add a click event on each of them
+                $navbarBurgers.forEach(function ($el) {
+                    $el.addEventListener('click', function () {
+
+                        // Get the target from the "data-target" attribute
+                        var target = $el.dataset.target;
+                        var $target = document.getElementById(target);
+
+                        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+                        $el.classList.toggle('is-active');
+                        $target.classList.toggle('is-active');
+
+                    });
+                });
+            }
+
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>

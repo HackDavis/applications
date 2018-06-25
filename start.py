8633b861371684b900ff28990ad7654c773117b0
Thomas Munduchira <thomas@thomasmunduchira.com>
2018-06-24 19:45:36 -0700

    Improve error-handling of script executor and simplify README (#4)
    * fixed semicolon bug, added .env processing
    
    * frontend boilerplate, adds npm install to install script
    
    * Squashed commit of the following:
    
    commit 4261435202f2c55ef3d30a771eb6fb2c644170b5
    Merge: 32c62c8 9543349
    Author: klanmiko <eternity01033@gmail.com>
    Date:   Wed Jun 20 22:48:02 2018 -0700
    
        Merge branch 'testing-setup'
    
    commit 954334937db560a4c0b12032d605b84ffdfdb440
    Author: Thomas Munduchira <thomas@thomasmunduchira.com>
    Date:   Wed Jun 20 22:45:38 2018 -0700
    
        Add travis config for continuous integration
    
    commit 32c62c83ac9b7da6298cb81f1a66d5cbbdb09b88
    Author: Kaelan Mikowicz <eternity01033@gmail.com>
    Date:   Wed Jun 20 22:28:28 2018 -0700
    
        fixed semicolon bug, added .env processing (#2)
    
    commit cdb96d76e47bcf5714979fe75f0f15850e0d3ab4
    Author: Thomas Munduchira <thomas@thomasmunduchira.com>
    Date:   Wed Jun 20 22:18:37 2018 -0700
    
        Add testing framework to requirements
    
    * Change start.sh to deploy.sh to prevent naming collision
    
    * Improve error-handling of script executor
    
    * Remove raw scripts from README
    
    * Make command argument for start script optional, with default of
    run-other
    
    Commands now have to be run with either a -c or --command flag as a side
    effect of making the argument optional.
    
    * Check for KeyboardInterrupt instead of generic exception
    
    * No need to strip whitespace since parser takes care of it for us

--- start.py
+++ start.py
@@ -1,21 +1,46 @@
-import subprocess
 import argparse
+import subprocess
+import sys
+
+
+'''
+exit_with_error prints an error to stderr and exits
+
+Parameters
+    error_message(str)
+'''
+
+
+def exit_with_error(error_message):
+    print(error_message, file=sys.stderr)
+    exit(1)
+
 
 commands = {
-    'all': "export FLASK_APP=app.py && flask run",
+    'run-other': "export FLASK_APP=app.py && flask run",
     'run-windows': "set FLASK_APP=app.py && flask run",
     'install': "pip3 install -r requirements.txt && cd applications-frontend && npm install && cd ..",
     'format': "autopep8 --in-place --aggressive --aggressive *.py",
-    'deploy': "pm2 start start.sh",
-    'publish-deps': 'pip3 freeze > requirements.txt'
-}
+    'deploy': "pm2 start deploy.sh",
+    'save-deps': 'pip3 freeze > requirements.txt'}
 
 parser = argparse.ArgumentParser(description='Manage the Flask App')
-parser.add_argument("command", help="[all | install | run-windows | format | deploy]", type=str)
+parser.add_argument(
+    "-c",
+    "--command",
+    default="run-other",
+    help="[run-other | run-windows | install | format | deploy | save-deps]",
+    type=str)  # optional, default is 'run-other'
 
 if __name__ == "__main__":
     args = parser.parse_args()
+    command = args.command
+
+    # check if command is a valid option
+    if command not in commands:
+        exit_with_error(f"command {command} is not valid!")
+
     try:
-        subprocess.run(commands[args.command], shell=True)
-    except:
-        pass # hush
+        subprocess.run(commands[command], shell=True)
+    except KeyboardInterrupt:
+        pass  # hush

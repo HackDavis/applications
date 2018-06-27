a2edf7409c755abf5ccda8638089372afe1d1adb
Kaelan Mikowicz <eternity01033@gmail.com>
2018-06-26 22:09:14 -0700

    Add routing to vue app, and header (#6)
    * fixed semicolon bug, added .env processing
    
    * frontend boilerplate, adds npm install to install script
    
    * Squashed commit of the following:
    
    commit 4261435202f2c55ef3d30a771eb6fb2c644170b5
    Merge: 32c62c8 9543349
    Author: klanmiko <eternity01033@gmail.com>
    Date:   Wed Jun 20 22:48:02 2018 -0700
    
        Merge branch 'testing-setup'
    
    commit 954334937db560a4c0b12032d605b84ffdfdb440
    Author: Thomas Munduchira <thomas@thomasmunduchira.com>
    Date:   Wed Jun 20 22:45:38 2018 -0700
    
        Add travis config for continuous integration
    
    commit 32c62c83ac9b7da6298cb81f1a66d5cbbdb09b88
    Author: Kaelan Mikowicz <eternity01033@gmail.com>
    Date:   Wed Jun 20 22:28:28 2018 -0700
    
        fixed semicolon bug, added .env processing (#2)
    
    commit cdb96d76e47bcf5714979fe75f0f15850e0d3ab4
    Author: Thomas Munduchira <thomas@thomasmunduchira.com>
    Date:   Wed Jun 20 22:18:37 2018 -0700
    
        Add testing framework to requirements
    
    * Change start.sh to deploy.sh to prevent naming collision
    
    * Improve error-handling of script executor
    
    * Remove raw scripts from README
    
    * server boilerplate code
    README update, start script separate install steps, gitignore vscode, update requirements.txt
    
    * remove helloworld and add vue router
    
    * header added, routing added, default page removed

--- applications-frontend/src/main.js
+++ applications-frontend/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import './sass/styles.sass'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')

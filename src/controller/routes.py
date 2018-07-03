0d02e327695c85f718b45c14de354b2708d75409
Thomas Munduchira <thomas@thomasmunduchira.com>
2018-07-02 21:38:24 -0700

    Limit static file controller to only GET requests

--- src/controller/routes.py
+++ src/controller/routes.py
@@ -8,6 +8,6 @@ static_path = os.path.join('applications-frontend', 'dist') # get directory of s
 @routes.route('/', defaults={'path': 'index.html'}) # redirect initial homepage requests to static files controller
 
 # return any requested static files
-@routes.route('/<path:path>')
+@routes.route('/<path:path>', methods=['GET'])
 def static_file(path):
     return send_from_directory(static_path, path)

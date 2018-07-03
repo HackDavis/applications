0d4cc137905234788bc99f2e7dca9264319a1e82
klanmiko <eternity01033@gmail.com>
2018-07-02 23:11:02 -0700

    added error route and view

--- applications-frontend/src/router.js
+++ applications-frontend/src/router.js
@@ -2,8 +2,24 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Applicant from './components/Applicant'
 import Home from './components/Home'
+import Error from './Error'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
 
 Vue.use(Router)
+
+Vue.http.interceptors.push(function() {
+  return function(response) {
+    if(response.status >= 400) {
+      this.$router.push({name:"error",
+      params: {
+        id: response.status,
+        message: response.body  
+      }});
+    }
+  }
+})
 
 let auth = {
   loggedIn() {
@@ -23,6 +39,12 @@ let router = new Router({
     {
       path: '/',
       component: Home
+    },
+    {
+      path:"/error/:id?/:message?",
+      name: "error",
+      component: Error,
+      props: true
     }
   ],
   mode: 'history'

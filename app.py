e9c58c618deed67ae2d8a16ff62dd799d8737ab4
Thomas Munduchira <thomas@thomasmunduchira.com>
2018-07-02 21:18:04 -0700

    Serve requested static files
    Using Blueprint to register routing operations in another file. http://flask.pocoo.org/docs/1.0/blueprints/.

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from dotenv import load_dotenv
-from src.review.routes import review_blueprint
 import os
+
+from src.review.routes import review_blueprint
+from src.controller.routes import routes
 
 load_dotenv()
 
 app = Flask(__name__)
 app.register_blueprint(review_blueprint)
+app.register_blueprint(routes)
+
---
+++ src/controller/routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, send_from_directory
+import os
+
+routes = Blueprint('routes', __name__)
+
+static_path = os.path.join('applications-frontend', 'dist') # get directory of static path
+
+@routes.route('/', defaults={'path': 'index.html'}) # redirect initial homepage requests to static files controller
+
+# return any requested static files
+@routes.route('/<path:path>')
+def static_file(path):
+    return send_from_directory(static_path, path)

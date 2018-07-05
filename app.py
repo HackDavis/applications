bb4fc3a24798596eaaf9ce45afcd9a6fed673258
klanmiko <eternity01033@gmail.com>
2018-07-04 20:32:29 -0700

    redo start commands

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from dotenv import load_dotenv
+from review.routes import review_blueprint
 import os
 import src.controller.routes
 
 load_dotenv()
 
 app = Flask(__name__)
-
-
+app.register_blueprint(review_blueprint)
--- src/review/routes.py
+++ src/review/routes.py
@@ -1,3 +1,3 @@
 from flask import Blueprint
 
-page = Blueprint()
+review_blueprint = Blueprint('review', __name__)

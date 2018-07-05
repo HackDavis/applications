369b49fff9b36493c0498b43c40135aeb9405fbd
Kaelan Mikowicz <eternity01033@gmail.com>
2018-07-04 20:36:14 -0700

    Move server options into .env (#7)
    * layout for applicant page with sidebar form
    
    * added error route and view
    
    * redo flask app
    
    * refactor route
    
    * redo start commands
    
    * redo start commands

--- applications-frontend/src/components/Applicant.vue
+++ applications-frontend/src/components/Applicant.vue
@@ -1,13 +1,61 @@
 <template>
-    <div>
+    <div class="container">
+        <div class="columns">
+            <aside class="review column is-one-quarter">
+                <form>
+                    <div class="field">
+                        <label class="label is-large">Your Score: </label>
+                        <div class="control is-large">
+                            <input class="input" type="number" :min="min" :max="max"/>
+                        </div>
+                    </div>
+                </form>
+            </aside>
+
+            <div class="column is-three-quarters">
+                <!-- random demographic information, can find a nice way to represent or table it -->
+                <section class="basicinfo">
+                    <h1 class="title">John Wick</h1>
+                    <h2 class="subtitle">Baba Yaga</h2>
+                </section>
+                <!-- responses go here -->
+                <section class="responses">
+                
+                </section>
+
+                <!-- random web links can go here -->
+                <section class="links">
+
+                </section>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            answer: 0,
+            min: 1,
+            max: 10
+        }
+    },
+    beforeRouteEnter(to, from, next) {
+        next(() => {
+        });
+    }
 }
 </script>
 
-<style>
+<style scoped>
+.basicinfo {
 
+}
+.responses {
+
+}
+.review {
+
+}
 </style>
--- applications-frontend/src/router.js
+++ applications-frontend/src/router.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Applicant from './components/Applicant'
+import Home from './components/Home'
+import Error from './Error'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
 
 Vue.use(Router)
+
+Vue.http.interceptors.push(function() {
+  return function(response) {
+    if(response.status >= 400) {
+      this.$router.push({name:"error",
+      params: {
+        id: response.status,
+        message: response.body  
+      }});
+    }
+  }
+})
 
 let auth = {
   loggedIn() {
@@ -18,6 +35,16 @@ let router = new Router({
       meta: {
         requiresAuth: true
       }
+    },
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path:"/error/:id?/:message?",
+      name: "error",
+      component: Error,
+      props: true
     }
   ],
   mode: 'history'

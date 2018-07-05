75ba4b9d4b584e65e0f2a454049e91d9b4339f8a
klanmiko <eternity01033@gmail.com>
2018-07-04 23:08:41 -0700

    connection pooling on database

--- src/db/connection.py
+++ src/db/connection.py
@@ -1,3 +1,25 @@
+import psycopg2
+import os
+pool = None
 def get_connection():
+    global pool
+    if not pool:
+        pool = psycopg2.pool.ThreadedConnectionPool(1, 4, dbname=os.getenv("DB_NAME"), user=os.getenv("DB_USER"), host=os.getenv("DB_HOSTNAME"), password=os.getenv("DB_PASSWORD"))
     """returns the current connection to the database. If none exists, create one"""
-    return None
+    return pool.getconn()
+
+def return_connection(conn):
+    global pool
+    if not pool:
+        raise Exception("connection not initialized")
+    pool.putconn(conn)
+
+def execute_query(query):
+    """returns a cursor from the executed query"""
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(query)
+
+    return_connection(conn)
+    return cursor

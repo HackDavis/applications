bcaa23b8e1f34e05073b1630eb24813db3690642
Thomas Munduchira <thomas@thomasmunduchira.com>
2018-07-08 11:43:33 -0700

    Get server to serve static files (#11)
    * Serve requested static files
    
    Using Blueprint to register routing operations in another file. http://flask.pocoo.org/docs/1.0/blueprints/.
    
    * Limit static file controller to only GET requests
    
    * Exit with 404 if invalid path given instead of unchecked exception
    
    * Split up integration tests and unit tests and add config for Tavern
    
    Tests are split up since only unit tests are going to be tested on Travis. Integration tests should be run locally before pushing code.
    
    * Add integration tests for static file serving
    
    * Reorganize controllers and update naming

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from dotenv import load_dotenv
-from src.review.routes import review_blueprint
 import os
+
+from src.controller.review import review
+from src.controller.static_files import static_files
 
 load_dotenv()
 
 app = Flask(__name__)
-app.register_blueprint(review_blueprint)
+app.register_blueprint(review)
+app.register_blueprint(static_files)

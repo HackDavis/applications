503b27d663426b6bbd0f4deeb446fde81e6515a7
Kaelan Mikowicz <eternity01033@gmail.com>
2018-07-14 15:24:58 -0700

    Add a user table (#10)
    * layout for applicant page with sidebar form
    
    * added error route and view
    
    * added sample csv file
    
    * connection pooling on database
    
    * saving csv into database
    
    * able to store the csv into the database
    
    * readme to add .env setup
    
    * add the id field of the user table and link it to the applications table
    
    * try catch around single query execution

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from dotenv import load_dotenv
-import os
 
 from src.controller.review import review
 from src.controller.static_files import static_files
+import src.db.models.UserModel as UserModel
 
 load_dotenv()
+
+UserModel.initialize_user_model()
 
 app = Flask(__name__)
 app.register_blueprint(review)

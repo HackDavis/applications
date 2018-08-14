9e259da40825fd8f24fa3f7752df576dc8b68664
Josh Arnold <josharnold997@gmail.com>
2018-08-13 19:01:19 -0700

    Fixed em, moved to radio buttons, deleted picture of applicant

--- applications-frontend/src/components/Applicant.vue
+++ applications-frontend/src/components/Applicant.vue
@@ -3,33 +3,50 @@
 	<div class="columns">
 
 		<!-- Control panel -->
-		<div class="column has-background-light is-3">		
+		<div class="column has-background-white is-3">		
 			<center>
-				<!-- Person's Image -->
-				<figure class="image is-128x128">
-					<img class="profile-picture" alt="" src="https://bulma.io/images/placeholders/256x256.png">
-				</figure>
+	
 
 				<!-- Person's name -->			
-				<div class="subtitle is-4" style="margin: 16px;">Full-name</div>
+				<div class="subtitle is-4" style="margin: 1em;">Full-name</div>
 
-				<!-- Break for looking good :D -->
-				<div class="pad-light"></div>
-				<div class="break"></div>
+				<!-- Break for looking good :D -->								
 				<div class="pad-light"></div>
 
 				<!-- Rating control -->
-				<div class="subtitle is-4" style="margin: 16px;">Rating</div>
-				<div class="buttons has-addons is-centered">
-					<span class="button">1</span>
-					<span class="button">2</span>
-					<span class="button">3</span>
-					<span class="button">4</span>
-					<span class="button">5</span>
+				<div class="subtitle is-4" style="margin: 1em;">Rating</div>
+
+				<div class="control-group">
+
+				<label class="control control--radio" >1
+				<input type="radio" name="radio" checked="checked"/>
+				<div class="control__indicator"></div>
+				</label>
+
+				<label class="control control--radio">2
+				<input type="radio" name="radio" checked="checked"/>
+				<div class="control__indicator"></div>
+				</label>	
+
+				<label class="control control--radio" >3
+				<input type="radio" name="radio" checked="checked"/>
+				<div class="control__indicator"></div>
+				</label>
+
+				<label class="control control--radio">4
+				<input type="radio" name="radio" checked="checked"/>
+				<div class="control__indicator"></div>
+				</label>	
+
+				<label class="control control--radio" >5
+				<input type="radio" name="radio" checked="checked"/>
+				<div class="control__indicator"></div>
+				</label>											
+
 				</div>
 
 				<!-- Next applicant button -->
-				<a class="button is-primary is-medium">Next applicant</a>
+				<a class="button is-primary is-medium" style="margin-top: 3em;">Next applicant</a>
 				<div class="pad"></div>			
 			</center>		
 		</div>
@@ -38,37 +55,36 @@
 		<div class="column has-background-light">
 
 			<!-- Major -->
-			<div class="title" style="margin: 16px;">Major</div>	
-			<div class="subtitle is-4" style="margin: 16px;">{{major}}</div>
-			<div class="pad-light"></div>		
+			<div class="title" style="margin: 1em;">Major</div>	
+			<div class="subtitle is-4" style="margin: 1em;">{{major}}</div>
+			<!-- <div class="pad-light"></div>		 -->
 
 			<!-- Year -->
-			<div class="title" style="margin: 16px;">College year</div>	
-			<div class="subtitle is-4" style="margin: 16px;">{{year}}</div>	
-			<div class="pad-light"></div>	
+			<div class="title" style="margin: 1em;">College year</div>	
+			<div class="subtitle is-4" style="margin: 1em;">{{year}}</div>	
+			<!-- <div class="pad-light"></div>	 -->
 
 			<!-- Essays -->
-			<div class="title" style="margin: 16px;">Text information</div>	
-			<div class="subtitle is-4" style="margin: 16px;">REPLACE ME</div>	
-			<div class="pad"></div>		
+			<div class="title" style="margin: 1em;">Text information</div>	
+			<div class="subtitle is-4" style="margin: 1em;">REPLACE ME</div>	
+			<!-- <div class="pad"></div>		 -->
 
 			<!-- Resume -->
-			<div class="title" style="margin: 16px;">Resume</div>			
-			<div>
-				<a class="button is-primary is-medium" style="margin-left: 16px;">Resume</a>					
-			</div>			
-			<iframe src="http://josharnoldjosh.github.io" width="80%" height="250px" style="overflow:auto; margin-top: 16px;"></iframe>
-			<div class="pad"></div>
+			<div class="title" style="margin: 1em; margin-bottom: 0em;">Resume</div>															
+			<iframe src="http://josharnoldjosh.github.io" width="80%" height="250em" style="overflow:auto; margin-top: 1em;"></iframe>
+			<!-- <div class="pad"></div> -->
 
-			<!-- Github -->
-			<div class="title" style="margin: 16px;">Github</div>	
-			<a class="button is-primary is-medium" style="margin-left: 16px;">Github</a>
-			<div class="pad"></div>	
+			<div class="title" style="margin: 1em;">Direct links</div>
+			<!-- Resume -->
+			<a class="button is-primary is-medium" style="margin-left: 1em;">Resume</a>	
 
-			<!-- LinkedIn -->
-			<div class="title" style="margin: 16px;">LinkedIn</div>	
-			<a class="button is-primary is-medium" style="margin-left: 16px;">LinkedIn</a>
-			<div class="pad"></div>													
+			<!-- Github -->			
+			<a class="button is-primary is-medium" style="margin-left: 1em;">Github</a>			
+
+			<!-- LinkedIn -->			
+			<a class="button is-primary is-medium" style="margin-left: 1em;">LinkedIn</a>							
+						
+			<div class="pad"></div>		
 		</div>
 	</div>
 </div>

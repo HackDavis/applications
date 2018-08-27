a10297250801822b4e13825a9ce42472f6522a88
Thomas Munduchira <thomas@thomasmunduchira.com>
2018-08-26 19:54:19 -0700

    Modify controller for reviewing applications to use the SQLAlchemy models

--- src/controller/review.py
+++ src/controller/review.py
@@ -1,37 +1,66 @@
-from flask import Blueprint, Response
-from ..lib import loadCSV
-from ..db.models import ApplicationsModel
-import os
+from flask import abort, Blueprint, jsonify, request, Response
+from flask_login import current_user, login_required
+
+from src.models.Answer import Answer
+from src.models.Application import Application
+from src.models.lib.Serializer import Serializer
+from src.shared import Shared
 
 review = Blueprint('review', __name__)
 
-_dirname = os.path.dirname(__file__)
+db = Shared.db
+google = Shared.google
+login_manager = Shared.login_manager
 
-@review.route('/configure/upload_csv')
-def doCSV():
-    with open(os.path.join(_dirname, "../lib/sample.csv"), encoding="utf-8") as data:
-        loadCSV.upload_csv(data)
-    return Response(status=200)
 
-@review.route('/review')
-def getApplicant():
-    model = ApplicationsModel.get_applications_model()
+@review.route('/review', methods=['GET'])
+@login_required
+def get_application():
+    """Get application for user"""
+    application = Application.get_application(current_user.get_id())
+    if application is None:
+        return Response('No more applications left to score', 200)
+
+    answers = Answer.get_answers(application.id)
+    response = {'application': application, 'answers': answers}
+    return jsonify(Serializer.serialize_value(response))
+
+
+@review.route('/review/skip', methods=['GET'])
+@login_required
+def skip_application():
+    """Skip application for user"""
+    past_application = Application.skip_application(current_user.get_id())
+    if past_application is None:
+        abort(400, 'User is not currently assigned an application')
+
+    application = Application.get_application(current_user.get_id())
+    if application is None:
+        return Response('No more applications left to score', 200)
+
+    answers = Answer.get_answers(application.id)
+    response = {'application': application, 'answers': answers}
+    return jsonify(Serializer.serialize_value(response))
+
+
+@review.route('/review/score', methods=['POST'])
+@login_required
+def score_application():
+    """Score application"""
+    score_str = request.args.get('score')
+    if score_str is None:
+        abort(400, 'score not provided')
+
     try:
-        data = model.getLockedApplicantRowForUser(1)
-        if data is None:
-            data = model.getNewLockedApplicantRow(1)
-    except Exception as e:
-        print("exception raised while getting locked applicant")
-        print(e)
-    return Response(str(data))
+        score = int(score_str)
+    except ValueError:
+        abort(400, 'score not an integer')
 
-@review.route('/review/skip')
-def skipApplicant():
-    model = ApplicationsModel.get_applications_model()
-    try:
-        model.skipApplicant(1)
-        data = model.getNewLockedApplicantRow(1)
-    except Exception as e:
-        print("could not skip applicant")
-        print(e)
-    return Response(str(data))
+    if score < 1 or score > 5:
+        abort(400, 'score not between 1 and 5')
+
+    application = Application.update_score(current_user.get_id(), score)
+    if application is None:
+        abort(400, 'User is not currently assigned an application')
+
+    return jsonify(Serializer.serialize_value(application))

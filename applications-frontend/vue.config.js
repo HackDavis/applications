bafddb12d869617f2f4888722402a2c7194c2a77
Kaelan Mikowicz <eternity01033@gmail.com>
2018-09-13 23:50:48 -0700

    use a proxy

---
+++ applications-frontend/vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    devServer: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:5000"
+            },
+            "/login": {
+                target: "http://localhost:5000"
+            },
+            "/logout": {
+                target: "http://localhost:5000"
+            },
+            "/google": {
+                target: "http://localhost:5000"
+            }
+        }
+    }
+}

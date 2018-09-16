4e14bf3642ff0cf10c389a8caf9e3b2e6e3d429d
Thomas Munduchira <thomas@thomasmunduchira.com>
2018-09-15 22:43:27 -0700

    Change reload endpoint and format Vue proxy config (#31)

--- src/controller/admin.py
+++ src/controller/admin.py
@@ -14,7 +14,7 @@ google = Shared.google
 login_manager = Shared.login_manager
 
 
-@admin.route('/configure/reload', methods=['POST'])
+@admin.route('/api/admin/reload', methods=['POST'])
 @login_required
 def reload():
     """Reload applications from CSV file."""

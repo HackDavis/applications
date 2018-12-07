6d91118bfddbaccf0db8e160be20791751c096f2
Kaelan Mikowicz <eternity01033@gmail.com>
2018-12-07 13:43:29 -0800

    accordion list

--- applications-frontend/src/components/Configure.vue
+++ applications-frontend/src/components/Configure.vue
@@ -15,21 +15,31 @@
             <button class="button" @click="reset">Reset</button>
         </div>
         <div class="column">
-            <div class="field is-grouped-multiline" v-for="weight in answer_weights" :key="weight[0]">
-                <label class="label">{{weight[1]}}</label>
-                <div class="control" v-for="w in weight[2]" :key="w.name">
-                    <label class="label">{{w.name}}</label>
-                    <input class="input" v-model="w.weight" />
+            <div class="accordions">
+                <div class="accordion" v-for="weight in answer_weights" :key="weight[0]">
+                    <div class="accordion-header toggle">{{weight[1]}}</div>
+                    <div class="accordion-body">
+                        <div class="accordion-content">
+                            <div class="control" v-for="w in weight[2]" :key="w.name">
+                                <label class="label">{{w.name}}</label>
+                                <input class="input" v-model="w.weight" />
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
-            </div>
+            </div>            
         </div>
-        <div class="column"></div>
     </div>
 </template>
 
 <script>
 import _ from 'lodash';
 import bulmaAccordion from 'bulma-accordion/dist/js/bulma-accordion.min.js';
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    bulmaAccordion.attach();
+});
 
 export default {
     data () {
@@ -58,5 +68,7 @@ export default {
 </script>
 
 <style>
-
+    .accordion-body {
+        overflow-y: scroll !important;
+    }
 </style>

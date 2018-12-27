94681c320e49c8c16bbb6bba83286c52af84c441
Kaelan Mikowicz <eternity01033@gmail.com>
2018-12-26 22:52:35 -0800

    fixes review routing and opening applicants in tabs

--- src/controller/static_files.py
+++ src/controller/static_files.py
@@ -1,5 +1,6 @@
 from flask import abort, Blueprint, send_from_directory
 from flask_login import login_required
+from flask import redirect
 import os
 
 static_files = Blueprint('static_files', __name__)
@@ -17,6 +18,6 @@ def static_file(path):
     """Return any requested static files"""
     full_path = os.path.join(static_path, path)
     if not os.path.exists(full_path):
-        abort(404, 'Path does not exist')
+        return send_from_directory(static_path, 'index.html')
 
     return send_from_directory(static_path, path)

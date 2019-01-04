fb75b71849195d22a68ddd3bf8c5960d464f78c3
Thomas Munduchira <thomas@thomasmunduchira.com>
2019-01-04 04:06:56 -0800

    Remove randomized quote from splash page

--- applications-frontend/src/components/Home.vue
+++ applications-frontend/src/components/Home.vue
@@ -5,7 +5,6 @@
         <img 
           class="image 3by4" 
           src="@/assets/Pinguino-Linux.svg">
-        <h1 class="title">{{ quote }}</h1>
       </div>
       
     </div>
@@ -19,27 +18,7 @@
 <script>
 import ApplicationsTable from './ApplicationsTable';
 
-/**
- * picks a random element from an array
- *
- * @param {array} array - the array to select an element from
- * @returns {object} an element from the array or null if the array is empty
- */
-function randomSelect(array) {
-  if (!array || array.length == 0) return null;
-  return array[Math.floor(Math.random() * array.length)];
-}
 export default {
-  data() {
-    return {
-      quotes: ["Papers, Please.", "This is boring.", "World cub"],
-      quote: ""
-    };
-  },
-  mounted() {
-    // demonstrate how to use lifecycle mounting, this will trigger whenever the page is relaoded
-    this.quote = randomSelect(this.quotes);
-  },
   components: {
     'applications-table': ApplicationsTable
   }

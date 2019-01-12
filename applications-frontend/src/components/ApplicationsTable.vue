e57422e8a42ed24c83e724d3082a38eeb24e9db3
Thomas Munduchira <thomas@thomasmunduchira.com>
2019-01-12 04:48:08 -0800

    Fix not being able to search properly by an applicant's name

--- applications-frontend/src/components/ApplicationsTable.vue
+++ applications-frontend/src/components/ApplicationsTable.vue
@@ -142,16 +142,18 @@ export default {
       }
 
       const searchTerm = this.searchTerm.toLowerCase();
-      const concatedName = this.concatName(app.firstName, app.lastName);
       const isAdmin = this.isUserAdmin();
 
-      return this.applications.filter(app =>
-        (concatedName && concatedName.toLowerCase().includes(searchTerm) ||
+      return this.applications.filter(app => {
+        const concatedName = this.concatName(app.firstName, app.lastName);
+        
+        return (concatedName.toLowerCase().includes(searchTerm) ||
         (app.email && app.email.includes(searchTerm)) ||
         (app.university && app.university.toLowerCase().includes(searchTerm)) ||
         (app.score && app.score.toString().includes(searchTerm)) ||
         (isAdmin && app.assignedToEmail && app.assignedToEmail.toLowerCase().includes(searchTerm)) ||
-        (app.lockedByEmail && app.lockedByEmail.toLowerCase().includes(searchTerm))));
+        (app.lockedByEmail && app.lockedByEmail.toLowerCase().includes(searchTerm)));
+      })
     },
     paginatedItems: function() {
       return this.searchResults.slice(0, this.maxViewIndex);
